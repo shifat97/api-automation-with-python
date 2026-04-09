4734bfd8ab2c25e0aced743d096136f82faadc84
shifat97 <shifatbinreza2@gmail.com>
2026-04-09 15:37:24 +0600

    change: separate files according to its api

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,3 @@
-import pytest
 import requests
 
 
@@ -13,37 +12,3 @@ def test_login(base_url, test_login_data):
 
     # Validating if authToken is empty or not
     assert response.json(), f"authToken is empty or None"
-
-
-# Testing get all students
-def test_get_students(base_url, auth_header, test_payload_structure):
-    # GET ALL STUDENTS
-    get_students = requests.get(f"{base_url}/api/student", headers=auth_header)
-
-    # Validating status code
-    assert get_students.status_code == 200, f"Expected 200, got {get_students.status_code}"
-
-    # Extract all student info
-    students = get_students.json()
-
-    # Validating structure like key and type
-    for student in students:
-        for key, expected_type in test_payload_structure.items():
-            # Extract type from key
-            key_type = type(student.get(key))
-
-            # Validating if key is available on student data
-            assert key in student, f"{key} is missing on student: {student}"
-
-            # Validating data type with data structure
-            assert key_type == expected_type, f"Expected {expected_type}, got {key_type}"
-
-            # Validating any key value is null or not
-            assert student[key] is not None, f"{key} should not be None"
-
-    # students[0]["_id"] = "qq"
-    # students[1]["_id"] = "qq"
-
-    # Validating unique _id
-    ids = [student["_id"] for student in students]
-    assert len(ids) == len(set(ids)), 'Duplicate student IDs found'
