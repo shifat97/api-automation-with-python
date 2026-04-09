d3e80f99cf8bfad0523ac4b437e1ee8f90f52149
shifat97 <shifatbinreza2@gmail.com>
2026-04-09 15:29:36 +0600

    add: validation for key in student data

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,10 @@
 import pytest
 import requests
 
+
 def test_login(base_url, test_login_data):
     response = requests.post(f"{base_url}/login", json=test_login_data)
-    
+
     # Validating status code
     assert response.status_code == 200, f"Expected 200, got {response.status_code}"
 
@@ -13,11 +14,12 @@ def test_login(base_url, test_login_data):
     # Validating if authToken is empty or not
     assert response.json(), f"authToken is empty or None"
 
-# Testing get all students   
+
+# Testing get all students
 def test_get_students(base_url, auth_header, test_payload_structure):
     # GET ALL STUDENTS
     get_students = requests.get(f"{base_url}/api/student", headers=auth_header)
-    
+
     # Validating status code
     assert get_students.status_code == 200, f"Expected 200, got {get_students.status_code}"
 
@@ -29,9 +31,16 @@ def test_get_students(base_url, auth_header, test_payload_structure):
         for key, expected_type in test_payload_structure.items():
             # Extract type from key
             key_type = type(student.get(key))
+
+            # Validating if key is available on student data
+            assert key in student, f"{key} is missing on student: {student}"
+
+            # Validating data type with data structure
             assert key_type == expected_type, f"Expected {expected_type}, got {key_type}"
+
+            # Validating any key value is null or not
             assert student[key] is not None, f"{key} should not be None"
-    
+
     # students[0]["_id"] = "qq"
     # students[1]["_id"] = "qq"
 
